33de876b0c79e9c6997cefb251ab0871e2a939aa
Xiao ChenYu <yunsn0303@gmail.com>
2012-03-21 00:28:18 +0800

    搞定用两张图片来做subview然后设置到view上面。

--- Draw Lock Login/Draw Lock.m
+++ Draw Lock Login/Draw Lock.m
@@ -8,13 +8,29 @@
 
 #import "Draw Lock.h"
 
+#define MATRIX_SIZE 3
+
 @implementation Draw_Lock
+
+
+-(void)setup
+{
+    self.backgroundColor = [UIColor darkGrayColor];
+}
+
+// 老头说过不直接调用initWithFrame 而是调用awakeFromNib来进行初始化
+// 进过测试 initWithFrame也不会被调用
+- (void)awakeFromNib
+{
+    [self setup];
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        NSLog(@"hello");
     }
     return self;
 }
@@ -51,6 +67,35 @@
     UIGraphicsPopContext();
 }
 
+- (void)setSubviewImageButton
+{
+    // 初始subview
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            UIImage *dotImage = [UIImage imageNamed:@"dot_off.png"];
+            UIImageView *imageView = [[UIImageView alloc] initWithImage:dotImage
+                                                       highlightedImage:[UIImage imageNamed:@"dot_on.png"]];
+            imageView.frame = CGRectMake(0, 0, dotImage.size.width, dotImage.size.height);
+            imageView.userInteractionEnabled = YES;
+            imageView.tag = j * MATRIX_SIZE + i + 1;
+            [self addSubview:imageView];
+        }
+    }
+    
+    // 重新设置每一个subview的位置
+    int w = self.frame.size.width / MATRIX_SIZE;
+    int h = self.frame.size.height / MATRIX_SIZE;
+    
+    int i = 0;
+    for (UIView *view in self.subviews)
+        if ([view isKindOfClass:[UIImageView class]]) {
+            int x = w * (i / MATRIX_SIZE) + w / 2;
+            int y = h * (i % MATRIX_SIZE) + h / 2;
+            view.center = CGPointMake(x, y);
+            i++;
+        }
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
@@ -59,10 +104,15 @@
     // Drawing code
     
     // 获取CGContext，注意UIKit里用的是一个专门的函数
-    CGContextRef context = UIGraphicsGetCurrentContext();
+    //
+    //CGContextRef context = UIGraphicsGetCurrentContext();
     
-    // draw background
-    [self drawBackgroundAtColor:0.0 atGreen:0.0 atBlue:0.0 atAphla:0.8 inContext:context];
+    // draw background 
+    //[self drawBackgroundAtColor:0.0 atGreen:0.0 atBlue:0.0 atAphla:0.8 inContext:context];
+    
+    // 蛋碎了 都想办法画了背景  然后发现view的背景可以这样设置 不过上面的是可以画成透明的
+    // self.backgroundColor = [UIColor darkGrayColor];
+    [self setSubviewImageButton];
     
     
 }
--- Draw Lock Login/ViewController.m
+++ Draw Lock Login/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+
+//#define MATRIX_SIZE 3
+
 @interface ViewController ()
 
 @end
@@ -18,8 +21,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    /*
+    for (int i=0; i<MATRIX_SIZE; i++) {
+        for (int j=0; j<MATRIX_SIZE; j++) {
+            UIImage *dotImage = [UIImage imageNamed:@"dot_off.png"];
+            UIImageView *imageView = [[UIImageView alloc] initWithImage:dotImage
+                                                       highlightedImage:[UIImage imageNamed:@"dot_on.png"]];
+            imageView.frame = CGRectMake(0, 0, dotImage.size.width, dotImage.size.height);
+            imageView.userInteractionEnabled = YES;
+            imageView.tag = j*MATRIX_SIZE + i + 1;
+            [self.view addSubview:imageView];
+        }
+    }
+     */
+
 }
 
+/*
+- (void)viewWillLayoutSubviews {
+
+    int w = self.view.frame.size.width/MATRIX_SIZE;
+    int h = self.view.frame.size.height/MATRIX_SIZE;
+    
+    int i=0;
+    for (UIView *view in self.view.subviews)
+        if ([view isKindOfClass:[UIImageView class]]) {
+            int x = w*(i/MATRIX_SIZE) + w/2;
+            int y = h*(i%MATRIX_SIZE) + h/2;
+            view.center = CGPointMake(x, y);
+            i++;
+        }
+    
+}
+*/
 - (void)viewDidUnload
 {
     [super viewDidUnload];
