3f4ea91573438e8c7395dd15f4331394aa30e76e
Xiao ChenYu <yunsn0303@gmail.com>
2012-03-20 21:33:07 +0800

    Initial Commit

---
+++ Draw Lock Login/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Draw Lock Login
+//
+//  Created by ChenYu Xiao on 12-3-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Draw Lock Login/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Draw Lock Login
+//
+//  Created by ChenYu Xiao on 12-3-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
