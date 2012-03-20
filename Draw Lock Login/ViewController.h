141836b472b38555cbcd0a65add30af0563e00ed
Xiao ChenYu <yunsn0303@gmail.com>
2012-03-21 00:59:38 +0800

    测试触摸函数

--- Draw Lock Login/ViewController.h
+++ Draw Lock Login/ViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    NSMutableArray  *paths;
+}
+
+@property (nonatomic, strong) NSMutableArray *paths;
+
 
 @end
--- Draw Lock Login/ViewController.m
+++ Draw Lock Login/ViewController.m
@@ -17,52 +17,49 @@
 
 @implementation ViewController
 
+@synthesize paths = _paths;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    /*
-    for (int i=0; i<MATRIX_SIZE; i++) {
-        for (int j=0; j<MATRIX_SIZE; j++) {
-            UIImage *dotImage = [UIImage imageNamed:@"dot_off.png"];
-            UIImageView *imageView = [[UIImageView alloc] initWithImage:dotImage
-                                                       highlightedImage:[UIImage imageNamed:@"dot_on.png"]];
-            imageView.frame = CGRectMake(0, 0, dotImage.size.width, dotImage.size.height);
-            imageView.userInteractionEnabled = YES;
-            imageView.tag = j*MATRIX_SIZE + i + 1;
-            [self.view addSubview:imageView];
-        }
-    }
-     */
-
+   
 }
 
-/*
-- (void)viewWillLayoutSubviews {
-
-    int w = self.view.frame.size.width/MATRIX_SIZE;
-    int h = self.view.frame.size.height/MATRIX_SIZE;
-    
-    int i=0;
-    for (UIView *view in self.view.subviews)
-        if ([view isKindOfClass:[UIImageView class]]) {
-            int x = w*(i/MATRIX_SIZE) + w/2;
-            int y = h*(i%MATRIX_SIZE) + h/2;
-            view.center = CGPointMake(x, y);
-            i++;
-        }
-    
-}
-*/
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (_paths == nil) {
+        _paths = [[NSMutableArray alloc] init];
+    }
+    NSLog(@"begin");
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"Move");
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"End");
+}
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return YES;
+    return NO;
+    /*
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return NO;
+    }
+     */
 }
 
 @end
